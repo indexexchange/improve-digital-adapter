10a3a72ccba9ab1f71468e2ce5e3a9a013df8169
unknown <a.gregorio@improvedigital.com>
2018-07-16 07:21:04 +0200

    Fix HTTP_SECURITY constant

--- improve-digital-htb.js
+++ improve-digital-htb.js
@@ -169,7 +169,7 @@ function ImproveDigitalHtb(configs) {
             }
 
             var protocol = Browser.getProtocol();
-            requestParameters.secure = protocol === "https:" ? __adServerClient.HTTP_SECURITY.CONSTANTS.SECURE : __adServerClient.CONSTANTS.HTTP_SECURITY.STANDARD;
+            requestParameters.secure = protocol === "https:" ? __adServerClient.CONSTANTS.HTTP_SECURITY.SECURE : __adServerClient.CONSTANTS.HTTP_SECURITY.STANDARD;
             var requestObject = [];
             for (var parcelCounter = 0; parcelCounter < returnParcels.length; parcelCounter++) {
                 var parcel = returnParcels[parcelCounter];
